ce397250ad1c17246e0253311bc036e7c8870c4b
Abhinav <Krabhinav216@gmail.com>
2024-07-17 15:22:10 +0530

    rearrangements done

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function AppRoute() {
   return (
     <Stack>
-      <Stack.Screen name="Home" />
+      <Stack.Screen name="(homeroute)" />
     </Stack>
   );
 }
